db5867775cfe4179331e30e16060eafbba8dcf81
Aman Belwal <belwal.aman92@gmail.com>
2025-02-09 15:13:45 -0500

    Added config.py and logger.py into project

---
+++ DevOps/config.py
@@ -0,0 +1,21 @@
+import logging
+
+# Configure logging
+logging.basicConfig(
+    filename="app.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+def log_message(level, message):
+    """Log messages at different levels"""
+    if level == "info":
+        logging.info(message)
+    elif level == "warning":
+        logging.warning(message)
+    elif level == "error":
+        logging.error(message)
+    elif level == "debug":
+        logging.debug(message)
+    else:
+        logging.info(message)
