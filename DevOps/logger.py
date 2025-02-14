bc98775d4d55f6b2e687436fe16f20b481b024a6
yohankhan <47495372+yohankhan@users.noreply.github.com>
2025-02-14 16:14:15 -0500

    Delete DevOps directory

--- DevOps/logger.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()  # Load environment variables from .env file
-
-class Config:
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://devuser:securepassword@localhost:27017/mydatabase")
-    TRANSCRIPTION_MODEL_PATH = os.getenv("MODEL_PATH", "models/vosk-model")
-    CHUNK_SIZE_MS = int(os.getenv("CHUNK_SIZE_MS", "10000"))
