ec4aee9df0d99c5078f063525089d50b6a587961
MarmikUpadhyay1 <marmikupadhyay111@gmail.com>
2025-02-23 16:29:37 -0500

    Add Signup_page.py and whisper_transcription.py

--- backend/whisper_transcription.py
+++ backend/whisper_transcription.py
@@ -2,15 +2,10 @@ from faster_whisper import WhisperModel
 
 model_size = "large-v3"
 
-# Run on GPU with FP16
-model = WhisperModel(model_size, device="cuda", compute_type="float16")
+# Run on CPU instead of GPU
+model = WhisperModel(model_size, device="cpu", compute_type="int8")
 
-# or run on GPU with INT8
-# model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
-# or run on CPU with INT8
-# model = WhisperModel(model_size, device="cpu", compute_type="int8")
-
-segments, info = model.transcribe("audio.mp3", beam_size=5)
+segments, info = model.transcribe("C:\Marmik\VidChap\backend\audio.mp3", beam_size=5)
 
 print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
 
@@ -18,7 +13,8 @@ for segment in segments:
     print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
 
 
-segments, _ = model.transcribe("audio.mp3")
+
+segments, _ = model.transcribe("C:\Marmik\VidChap\backend\audio.mp3")
 segments = list(segments)  # The transcription will actually run here.
 
 
